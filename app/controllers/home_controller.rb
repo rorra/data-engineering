99682cef29eab9e070e631468a84795ea248d5cc
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-11-28 05:38:53 -0300

    Small refactoring, top bar for login/registration and logout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,9 @@
 class HomeController < ApplicationController
+  def index
+    if user_signed_in?
+      render
+    else
+      render action: 'index_not_signed_in'
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,14 @@
 class User
   include Mongoid::Document
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 
   ## Database authenticatable
   field :email,              :type => String, :default => ""
   field :encrypted_password, :type => String, :default => ""
 
-  validates_presence_of :email
-  validates_presence_of :encrypted_password
-  
   ## Recoverable
   field :reset_password_token,   :type => String
   field :reset_password_sent_at, :type => Time
@@ -27,17 +23,11 @@ class User
   field :current_sign_in_ip, :type => String
   field :last_sign_in_ip,    :type => String
 
-  ## Confirmable
-  # field :confirmation_token,   :type => String
-  # field :confirmed_at,         :type => Time
-  # field :confirmation_sent_at, :type => Time
-  # field :unconfirmed_email,    :type => String # Only if using reconfirmable
+  ## Custom fields
+  field :name
 
-  ## Lockable
-  # field :failed_attempts, :type => Integer, :default => 0 # Only if lock strategy is :failed_attempts
-  # field :unlock_token,    :type => String # Only if unlock strategy is :email or :both
-  # field :locked_at,       :type => Time
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :encrypted_password, presence: true
+  validates :name, presence: true
 
-  ## Token authenticatable
-  # field :authentication_token, :type => String
 end
